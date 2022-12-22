dd417cccf1eea750c5952b82493b9bd7cbd9dfbf
Shubam Virdi <shubam.virdi@cloverbaytechnologies.com>
2022-12-22 17:26:00 +0530

    Spring Docs project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,15 +4,22 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -35,6 +42,19 @@ public class DemoApplication {
 
 
     @Bean
+    @RouterOperations(
+            @RouterOperation(
+                    path = "/authenticated/request",
+                    method = RequestMethod.POST,
+                    operation = @Operation(
+                            operationId = "some_operation_id",
+                            description = "to create",
+                            tags = "API",
+                            responses = @ApiResponse(responseCode = "200",
+                                    content = @Content(schema = @Schema(implementation = CreateRequest.class))
+                            )
+                    ))
+    )
     public Function<Message<Void>, Message<CreateRequest>> create(){
         return request -> new GenericMessage<>(new CreateRequest(true), headers);
     }
